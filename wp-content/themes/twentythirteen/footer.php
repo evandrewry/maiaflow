cd2687754ae9d4762187c0da45d15614cdb92357
maiaflow <maiacanter@gmail.com>
2015-09-23 15:13:24 -0400

    footer refix

--- wp-content/themes/twentythirteen/footer.php
+++ wp-content/themes/twentythirteen/footer.php
@@ -13,7 +13,10 @@
 		</div><!-- #main -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<?php get_sidebar( 'main' ); ?>
+      <div class="site-info">
 
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
+      </div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 	<?php wp_footer(); ?>
