2913aa5209140e443d88ea9e48103be2b88b037f
maiaflow <maiacanter@gmail.com>
2015-09-23 21:22:47 -0400

    scroll2?

--- wp-content/themes/twentythirteen/js/functions.js
+++ wp-content/themes/twentythirteen/js/functions.js
@@ -137,7 +137,7 @@
     postIndex++;
   });
   $(window).scroll(function(event) {
-    $('.col.middle').css('margin-top', Math.max(0,$(this).scrollTop()/3 - $(this).height()/5))
+    $('.col.middle').css('margin-top', Math.max(0,$(this).scrollTop()/2 - $(this).height()/5))
   });
   window.onInfiniteScrollDefault = function () {
     $('.infinite-wrap .post').each(function(index) {
